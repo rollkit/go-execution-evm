cab390bd86e207fb31fb66795faf754b68d76b0c
Janani Anbarasan <janani.anbarasan@gmail.com>
2024-11-10 09:37:46 -0600

    programatically setup docker dependencies for integration tests

--- go.mod
+++ go.mod
@@ -24,9 +24,16 @@ require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/docker v27.3.1+incompatible // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
 	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
 	github.com/ethereum/go-verkle v0.1.1-0.20240829091221-dffa7562dbe9 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -45,6 +52,7 @@ require (
 	github.com/minio/sha256-simd v1.0.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/multiformats/go-base32 v0.1.0 // indirect
 	github.com/multiformats/go-base36 v0.2.0 // indirect
@@ -55,6 +63,9 @@ require (
 	github.com/multiformats/go-multihash v0.2.3 // indirect
 	github.com/multiformats/go-multistream v0.5.0 // indirect
 	github.com/multiformats/go-varint v0.0.7 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rs/cors v1.11.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
@@ -62,6 +73,10 @@ require (
 	github.com/supranational/blst v0.3.13 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.56.0 // indirect
+	go.opentelemetry.io/otel v1.31.0 // indirect
+	go.opentelemetry.io/otel/metric v1.31.0 // indirect
+	go.opentelemetry.io/otel/trace v1.31.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
---
+++ integration_test.go
@@ -0,0 +1,141 @@
+package execution
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+
+	proxy_json_rpc "github.com/rollkit/go-execution/proxy/jsonrpc"
+	rollkit_types "github.com/rollkit/go-execution/types"
+)
+
+const (
+	TEST_ETH_URL    = "http://localhost:8545"
+	TEST_ENGINE_URL = "http://localhost:8551"
+
+	CHAIN_ID     = "1234"
+	GENESIS_HASH = "0x8bf225d50da44f60dee1c4ee6f810fe5b44723c76ac765654b6692d50459f216"
+	JWT_SECRET   = "09a23c010d96caaebb21c193b85d30bbb62a9bac5bd0a684e9e91c77c811ca65"
+
+	DOCKER_CHAIN_PATH      = "./docker/chain" // path relative to the test file
+	DOCKER_JWTSECRET_PATH  = "./docker/jwttoken/" // path relative to the test file
+	DOCKER_JWT_SECRET_FILE = "testsecret.hex"
+)
+
+func setupTestRethEngine(t *testing.T) func() {
+	t.Helper()
+
+	chainPath, err := filepath.Abs(DOCKER_CHAIN_PATH) 
+	require.NoError(t, err)
+
+	jwtSecretPath, err := filepath.Abs(DOCKER_JWTSECRET_PATH) 
+	require.NoError(t, err)
+
+	err = os.WriteFile(DOCKER_JWTSECRET_PATH+DOCKER_JWT_SECRET_FILE, []byte(JWT_SECRET), 0644)
+	require.NoError(t, err)
+
+	cli, err := client.NewClientWithOpts()
+	require.NoError(t, err)
+
+	rethContainer, err := cli.ContainerCreate(context.Background(),
+		&container.Config{
+			Image:      "ghcr.io/paradigmxyz/reth",
+			Entrypoint: []string{"/bin/sh", "-c"},
+			Cmd: []string{
+				`
+				reth init --chain /root/chain/genesis.json && \
+          		reth node \
+          		--chain /root/chain/genesis.json \
+          		--metrics 0.0.0.0:9001 \
+          		--log.file.directory /root/logs \
+          		--authrpc.addr 0.0.0.0 \
+          		--authrpc.port 8551 \
+          		--authrpc.jwtsecret /root/jwt/testsecret.hex \
+          		--http --http.addr 0.0.0.0 --http.port 8545 \
+          		--http.api "eth,net,web3" \
+          		--disable-discovery \
+          		--debug.tip 0x8bf225d50da44f60dee1c4ee6f810fe5b44723c76ac765654b6692d50459f216 \
+          		-vvvv
+				`,
+			},
+			ExposedPorts: map[nat.Port]struct{}{
+				nat.Port("8545/tcp"): {},
+				nat.Port("8551/tcp"): {},
+			},
+		},
+		&container.HostConfig{
+			Binds: []string{
+				chainPath + ":/root/chain:ro",
+				jwtSecretPath + ":/root/jwt:ro",
+			},
+			PortBindings: nat.PortMap{
+				nat.Port("8545/tcp"): []nat.PortBinding{
+					{
+						HostIP:   "0.0.0.0",
+						HostPort: "8545",
+					},
+				},
+				nat.Port("8551/tcp"): []nat.PortBinding{
+					{
+						HostIP:   "0.0.0.0",
+						HostPort: "8551",
+					},
+				},
+			},
+		},
+		nil, nil, "reth")
+	require.NoError(t, err)
+
+	err = cli.ContainerStart(context.Background(), rethContainer.ID, container.StartOptions{})
+	require.NoError(t, err)
+
+	// a reasonable time to wait for the container to start!
+	// do we want a more predictable elaborate code to wait for the container to be running?
+	time.Sleep(50 * time.Millisecond) 
+
+	return func() {
+		err = cli.ContainerStop(context.Background(), rethContainer.ID, container.StopOptions{})
+		require.NoError(t, err)
+		err = cli.ContainerRemove(context.Background(), rethContainer.ID, container.RemoveOptions{})
+		require.NoError(t, err)
+		err = os.Remove(DOCKER_JWTSECRET_PATH+DOCKER_JWT_SECRET_FILE)
+
+		require.NoError(t, err)
+	}
+}
+
+func TestEngineAPIExecutionClient_engineLifecycle(t *testing.T) {
+	defer setupTestRethEngine(t)()
+
+	genesisHash := common.HexToHash(GENESIS_HASH)
+	client, err := NewEngineAPIExecutionClient(
+		&proxy_json_rpc.Config{},
+		TEST_ETH_URL,
+		TEST_ENGINE_URL,
+		JWT_SECRET,
+		genesisHash,
+		common.Address{},
+	)
+	require.NoError(t, err)
+
+	genesisTime := time.Now().UTC().Truncate(time.Second)
+	initialHeight := uint64(0)
+
+	stateRoot, gasLimit, err := client.InitChain(genesisTime, initialHeight, CHAIN_ID)
+	require.NoError(t, err)
+
+	staterootHash := common.HexToHash("0x362b7d8a31e7671b0f357756221ac385790c25a27ab222dc8cbdd08944f5aea4")
+	var expectedStateRoot rollkit_types.Hash
+	copy(expectedStateRoot[:], staterootHash.Bytes())
+
+	require.Equal(t, expectedStateRoot, stateRoot)
+	require.Equal(t, uint64(1000000), gasLimit)
+}
